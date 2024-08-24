767d1ce323f935ca4396758baad2146ab54f9481
Abhishek Ingle <abhishekingle662@gmail.com>
2024-08-24 12:48:00 -0400

    code add

---
+++ components/footer.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import '../styles/footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        
+        <p>© {new Date().getFullYear()} - Designed and developed by [Abhishek Ingle]</p>
+        <p>"Building the future with code and creativity."</p>
+        
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
